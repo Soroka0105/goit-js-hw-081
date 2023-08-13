f2d36895c31796a27a5c5cb9298ef43385bdcbfe
Soroka0105 <soroka0105@gmail.com>
2023-08-13 13:02:06 -0400

    asdsa

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+const parsedData = JSON.parse(savedData);
+
+form.addEventListener('input', throttle(formData, 500));
+function formData() {
+  const data = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+  //   console.log(parsedData.email);
+}
+form.elements.email.value = parsedData.email;
+form.elements.message.value = parsedData.message;
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+  console.log(parsedData);
+  form.reset();
+  localStorage.clear();
+}
